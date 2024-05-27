3504445d6805c9d521fa1d596ca33efecbf8a0b2
canxi4589 <caotri1203@gmail.com>
2024-05-27 21:59:28 +0700

    JWT authentication and authorization

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Model.Models;
+
+namespace Repository
+{
+    public interface IUserRepository
+    {
+        User Add(User entity);
+        void Delete(int id);
+        User GetById(int id);
+        List<User> GetAll();
+        User Update(User entity);
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using DAO;
+using Microsoft.EntityFrameworkCore;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        //Dependency Injection.You can find this in program.cs
+        private readonly DIAMOND_DBContext _context;
+
+        public UserRepository(DIAMOND_DBContext context)
+        {
+            this._context = context;
+        }
+        public User Add(User entity)
+        {
+            _context.Users.Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public void Delete(int id)
+        {
+            User user = GetById(id);
+            if (user != null)
+            {
+                user.Status = "disabled";
+                _context.SaveChanges();
+            }
+        }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.FirstOrDefault(c => c.UserId == id);
+        }
+
+        public User Update(User entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+            return entity;
+        }
+
+    }
+}
