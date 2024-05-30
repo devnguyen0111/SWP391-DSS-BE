956ea61d1b3e9e2e4420639eb589bd9e4db39d26
PhucHong-6113 <phuongdubu@gmail.com>
2024-05-30 21:51:15 +0700

    update diamond post

--- DiamondShopSystem.API/Controllers/DiamondController.cs
+++ DiamondShopSystem.API/Controllers/DiamondController.cs
@@ -1,4 +1,5 @@
 ﻿
+using DiamondShopSystem.API.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Model.Models;
@@ -17,7 +18,7 @@ namespace DiamondShopSystem.API.Controllers
             _diamondService = diamondService;
         }
 
-        [HttpGet]
+        [HttpGet("diamonds")]
         public ActionResult<IEnumerable<Diamond>> GetDiamonds()
         {
             return _diamondService.GetAllDiamonds();
@@ -36,19 +37,27 @@ namespace DiamondShopSystem.API.Controllers
             return diamond;
         }
 
-        
+        [HttpPost]
+        public ActionResult<Diamond> PostDiamomnd([FromBody] DTO.DiamondRequest diamond)
+        {
+            var newDiamond = new Diamond(diamond.DiamondName, diamond.CaratWeight, diamond.Color, diamond.Clarity, diamond.Cut, diamond.Shape, diamond.Price);
+
+            _diamondService.AddDiamond(newDiamond);
+            return Ok(newDiamond);
+        }
 
         [HttpPut("{id}")]
-        public IActionResult PutDiamond(int id, Diamond diamond)
+        public IActionResult PutDiamond(int id, [FromBody] Diamond diamondDTO)
         {
-            if (id != diamond.DiamondId)
+            if (id != diamondDTO.DiamondId)
             {
                 return BadRequest();
             }
+            var newDiamond = new Diamond(diamondDTO.DiamondName, diamondDTO.CaratWeight, diamondDTO.Color, diamondDTO.Clarity, diamondDTO.Cut, diamondDTO.Shape, diamondDTO.Price);
 
             try
             {
-                _diamondService.UpdateDiamond(diamond);
+                _diamondService.UpdateDiamond(newDiamond);
             }
             catch (DbUpdateConcurrencyException)
             {
