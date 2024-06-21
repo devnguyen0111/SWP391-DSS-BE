4cd6bed6034ed76eb776ddbb9b81dcc2d7f706e2
canxi4589 <124656329+canxi4589@users.noreply.github.com>
2024-06-21 21:03:28 +0700

    ptsd

--- DiamondShopSystem.API/DTO/OrderHistoryResponse.cs
+++ DiamondShopSystem.API/DTO/OrderHistoryResponse.cs
@@ -5,25 +5,11 @@ namespace DiamondShopSystem.API.DTO
     public class OrderHistoryResponse
     {
         public int OrderId { get; set; }
-
         public DateTime OrderDate { get; set; }
-
         public string Status { get; set; }
-
-        public string ShippingMethodName { get; set; }
-
-        public string ProductName { get; set; }
-
-        public string SizeName { get; set; }
-
-        public string MetaltypeName { get; set; }
-
-        public string DiamondName { get; set; }
-
         public decimal TotalAmount { get; set; }
-
+        public string ShippingMethodName { get; set; }
         public List<OrderHistoryItem> Items { get; set; }
-
         public OrderHistoryResponse()
         {
             Items = new List<OrderHistoryItem>();
@@ -34,6 +20,9 @@ namespace DiamondShopSystem.API.DTO
         public int PId { get; set; }
         public string Name { get; set; }
         public decimal Total {get; set; }
+        public string SizeName { get; set; }
+        public string MetaltypeName { get; set; }
+        public string DiamondName { get; set; }
         public string Img { get; set; }
     }
 
