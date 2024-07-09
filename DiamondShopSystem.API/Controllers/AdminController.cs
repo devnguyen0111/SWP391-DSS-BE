c4e8a6486d6082a0919ce6bbf13617ed2c7ae2a9
Nhứt Nguyên <kieuphuongcsgo@gmail.com>
2024-07-09 21:08:01 +0700

    Refactor and enhance AdminController
    - Refactored Dependency Injection in `AdminController` to align with service-oriented architecture by removing `Repository` namespaces and adding `Services.Users`, `Services.Diamonds`, and `Services.Products`.
    - Enhanced `AdminController` constructor to include `IOrderService`, enabling order management functionalities.
    - Added new endpoints for order management: `GetAllOrders` for retrieving all orders and `TotalRevenue` for calculating total revenue from paid orders, enhancing administrative capabilities.

--- DiamondShopSystem.API/Controllers/AdminController.cs
+++ DiamondShopSystem.API/Controllers/AdminController.cs
@@ -1,10 +1,11 @@
 ﻿using DiamondShopSystem.API.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Model.Models;
-using Repository;
-using Repository.Users;
+using Services.Users;
 using Services.Admin;
-
+using Services.Diamonds;
+using Services.Products;
+using Repository.Users;
 namespace DiamondShopSystem.API.Controllers
 {
     [ApiController]
@@ -13,10 +14,12 @@ namespace DiamondShopSystem.API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IAdminService _adminService;
-        public AdminController(IUserRepository userRepository, IAdminService adminService)
+        private readonly IOrderService _orderService;
+        public AdminController(IUserRepository userRepository, IAdminService adminService, IOrderService orderService)
         {
             _userRepository = userRepository;
             _adminService = adminService;
+            _orderService = orderService;
         }
 
         [HttpGet("users")]
@@ -52,6 +55,25 @@ namespace DiamondShopSystem.API.Controllers
             {
                 return NotFound("There is NO Such User");
             }
+        }
+
+        [HttpGet("GetAllOrders")]
+        public IActionResult GetAllOrders()
+        {
+            var orders = _orderService.getAllOrders();
+            return Ok(orders);
+        }
+
+        [HttpGet("TotalRevenue")]
+        public IActionResult GetTotalRevenue()
+        {
+            // Fetch all orders with status "Paid"
+            var paidOrders = _orderService.getAllOrders().Where(order => order.Status == "Paid");
+
+            // Calculate total revenue
+            decimal totalRevenue = paidOrders.Sum(order => order.TotalAmount);
+
+            return Ok(new { TotalRevenue = totalRevenue });
         }
     }
 }
