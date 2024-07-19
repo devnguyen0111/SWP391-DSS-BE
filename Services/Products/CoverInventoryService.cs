5d8f15057c7c27597172cf11be64245deeafe02b
canxi4589 <124656329+canxi4589@users.noreply.github.com>
2024-07-19 12:30:31 +0700

    minor conflict

--- Services/Products/CoverInventoryService.cs
+++ Services/Products/CoverInventoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Repository.Products;
+using Services.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace Services.Products
 {
-    internal class CoverInventoryService
+    public class CoverInventoryService
     {
+        private readonly ICoverInventoryRepository coverInventoryRepository;
+        private readonly IDisableService disableService;
+
+        public CoverInventoryService(ICoverInventoryRepository coverInventoryRepository, IDisableService disableService)
+        {
+            this.coverInventoryRepository = coverInventoryRepository;
+            this.disableService = disableService;
+        }
+
     }
 }
